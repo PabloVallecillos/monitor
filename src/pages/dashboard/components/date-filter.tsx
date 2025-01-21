8530fcdf956b4446fbb375fa142ab93c9cc5b8e1
PabloVallecillos <vallecillospablo@gmail.com>
2025-01-21 22:23:12 +0100

    feat: Add charts
    refs: 8697jm8gb

--- src/pages/dashboard/components/date-filter.tsx
+++ src/pages/dashboard/components/date-filter.tsx
@@ -1,18 +1,17 @@
 import * as Popover from '@radix-ui/react-popover';
 import React, { useState, useEffect } from 'react';
+import { useTheme } from '@/providers/theme-provider';
 
 const DateFilter: React.FC = () => {
   const today = new Date();
-
   const startDateDefault = new Date(today);
   startDateDefault.setDate(today.getDate() - 28);
-
   const formatDate = (date: Date) => date.toISOString().split('T')[0];
-
   const [startDate, setStartDate] = useState<string>(
     formatDate(startDateDefault)
   );
   const [endDate, setEndDate] = useState<string>(formatDate(today));
+  const { theme } = useTheme();
 
   useEffect(() => {
     setStartDate(formatDate(startDateDefault));
@@ -22,7 +21,7 @@ const DateFilter: React.FC = () => {
   return (
     <div className="flex flex-col items-center gap-4 md:flex-row">
       <div>
-        <label className="block text-sm font-medium text-muted-foreground">
+        <label className="mb-1 block text-sm font-medium text-muted-foreground">
           Start Date
         </label>
         <Popover.Root>
@@ -31,7 +30,7 @@ const DateFilter: React.FC = () => {
               type="date"
               value={startDate}
               onChange={(e) => setStartDate(e.target.value)}
-              className="w-full rounded-md border border-gray-300 p-2 text-sm focus:outline-none focus:ring-2 focus:ring-primary"
+              className={`w-full rounded-md border border-gray-300 p-2 text-sm focus:outline-none focus:ring-2 focus:ring-primary ${theme !== 'light' ? 'text-black' : 'text-primary'}`}
               placeholder={startDate ? '' : 'Select start date'}
             />
           </Popover.Trigger>
@@ -39,7 +38,7 @@ const DateFilter: React.FC = () => {
       </div>
 
       <div>
-        <label className="block text-sm font-medium text-muted-foreground">
+        <label className="mb-1 block text-sm font-medium text-muted-foreground">
           End Date
         </label>
         <Popover.Root>
@@ -48,7 +47,7 @@ const DateFilter: React.FC = () => {
               type="date"
               value={endDate}
               onChange={(e) => setEndDate(e.target.value)}
-              className="w-full rounded-md border border-gray-300 p-2 text-sm focus:outline-none focus:ring-2 focus:ring-primary"
+              className={`w-full rounded-md border border-gray-300 p-2 text-sm focus:outline-none focus:ring-2 focus:ring-primary ${theme !== 'light' ? 'text-black' : 'text-primary'}`}
               placeholder={endDate ? '' : 'Select end date'}
             />
           </Popover.Trigger>
